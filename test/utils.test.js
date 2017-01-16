a08c781e0bb4ea9db9dff99fc0e2b2020b213e4e
PeterEB <peter.eb9@gmail.com>
2017-01-16 11:57:27 +0800

    refactoring

--- test/utils.test.js
+++ test/utils.test.js
@@ -246,7 +246,7 @@ describe('Netcore Drivers Check', function () {
         var clientName,
             devRegHdlr = function (msg) {
                 switch(msg.type) {
-                    case 'registered':
+                    case 'devIncoming':
                         clientName = msg.data.clientName;
                         permAddr1 = msg.data.mac + '/' + msg.data.clientName;
                         mac = msg.data.mac;
@@ -279,7 +279,7 @@ describe('Netcore Drivers Check', function () {
         var clientName,
             devReRegHdlr = function (msg) {
                 switch(msg.type) {
-                    case 'registered':
+                    case 'devIncoming':
                         clientName = msg.data.clientName;
                         permAddr1 = msg.data.mac + '/' + msg.data.clientName;
 
@@ -314,7 +314,7 @@ describe('Netcore Drivers Check', function () {
         var clientName,
             devSecRegHdlr = function (msg) {
                 switch(msg.type) {
-                    case 'registered':
+                    case 'devIncoming':
                         clientName = msg.data.clientName;
                         permAddr2 = msg.data.mac + '/' + msg.data.clientName;
 
@@ -339,7 +339,7 @@ describe('Netcore Drivers Check', function () {
         var clientName,
             devThiRegHdlr = function (msg) {
                 switch(msg.type) {
-                    case 'registered':
+                    case 'devIncoming':
                         clientName = msg.data.clientName;
                         permAddr3 = msg.data.mac + '/' + msg.data.clientName;
 
@@ -375,7 +375,8 @@ describe('Device Drivers Check', function () {
     this.timeout(10000);
 
     it('read()', function (done) {
-        nc.devRead(permAddr1, 'manufacturer', function (err, result) {
+
+        nc._findDriver('dev', 'read')(permAddr1, 'manufacturer', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 'sivann')
@@ -384,13 +385,13 @@ describe('Device Drivers Check', function () {
     });
 
     it('read() - attr not exist', function (done) {
-        nc.devRead(permAddr1, 'foo', function (err, result) {
+        nc._findDriver('dev', 'read')(permAddr1, 'foo', function (err, result) {
             if (err) done();
         });
     });
 
     it('read() - device2', function (done) {
-        nc.devRead(permAddr2, 'manufacturer', function (err, result) {
+        nc._findDriver('dev', 'read')(permAddr2, 'manufacturer', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 'freebird')
@@ -399,7 +400,7 @@ describe('Device Drivers Check', function () {
     });
 
     it('read() - device3', function (done) {
-        nc.devRead(permAddr3, 'model', function (err, result) {
+        nc._findDriver('dev', 'read')(permAddr3, 'model', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 'coap-7688')
@@ -408,11 +409,11 @@ describe('Device Drivers Check', function () {
     });
 
     it('write()', function (done) {
-        nc.devWrite(permAddr1, 'serial', 'c-0001', function (err, result) {
+        nc._findDriver('dev', 'write')(permAddr1, 'serial', 'c-0001', function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.devRead(permAddr1, 'serial', function (err, result) {
+                nc._findDriver('dev', 'read')(permAddr1, 'serial', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 'c-0001')
@@ -423,17 +424,17 @@ describe('Device Drivers Check', function () {
     });
 
     it('write() - attr not exist', function (done) {
-        nc.devWrite(permAddr1, 'foo', 'c-0001', function (err, result) {
+        nc._findDriver('dev', 'write')(permAddr1, 'foo', 'c-0001', function (err, result) {
             if (err) done();
         });
     });
 
     it('write() - device2', function (done) {
-        nc.devWrite(permAddr2, 'serial', 'c-0002', function (err, result) {
+        nc._findDriver('dev', 'write')(permAddr2, 'serial', 'c-0002', function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.devRead(permAddr2, 'serial', function (err, result) {
+                nc._findDriver('dev', 'read')(permAddr2, 'serial', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 'c-0002')
@@ -444,11 +445,11 @@ describe('Device Drivers Check', function () {
     });
 
     it('write() - device3', function (done) {
-        nc.devWrite(permAddr3, 'serial', 'c-0003', function (err, result) {
+        nc._findDriver('dev', 'write')(permAddr3, 'serial', 'c-0003', function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.devRead(permAddr3, 'serial', function (err, result) {
+                nc._findDriver('dev', 'read')(permAddr3, 'serial', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 'c-0003')
@@ -463,7 +464,7 @@ describe('Gadget Drivers Check', function () {
     this.timeout(10000);
 
     it('read()', function (done) {
-        nc.gadRead(permAddr1, 'temperature/0', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'read')(permAddr1, 'temperature/0', 'sensorValue', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 21) {
@@ -473,19 +474,19 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('read() - not found', function (done) {
-        nc.gadRead(permAddr1, 'temperature/0', 'foo', function (err, result) {
+        nc._findDriver('gad', 'read')(permAddr1, 'temperature/0', 'foo', function (err, result) {
             if (err) done();
         });
     });
 
     it('read() - not allowed', function (done) {
-        nc.gadRead(permAddr1, 'temperature/0', '5703', function (err, result) {
+        nc._findDriver('gad', 'read')(permAddr1, 'temperature/0', '5703', function (err, result) {
             if (err) done();
         });
     });
 
     it('read() - device2', function (done) {
-        nc.gadRead(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'read')(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 56) {
@@ -495,7 +496,7 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('read() - device3', function (done) {
-        nc.gadRead(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'read')(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
             if (err) {
                 console.log(err);
             } else if (result === 87) {
@@ -505,11 +506,11 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('write()', function (done) {
-        nc.gadWrite(permAddr1, 'temperature/0', 'sensorValue', 25, function (err, result) {
+        nc._findDriver('gad', 'write')(permAddr1, 'temperature/0', 'sensorValue', 25, function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.gadRead(permAddr1, 'temperature/0', 'sensorValue', function (err, result) {
+                nc._findDriver('gad', 'read')(permAddr1, 'temperature/0', 'sensorValue', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 25) {
@@ -521,11 +522,11 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('write() - device2', function (done) {
-        nc.gadWrite(permAddr2, 'humidity/0', 'sensorValue', 55, function (err, result) {
+        nc._findDriver('gad', 'write')(permAddr2, 'humidity/0', 'sensorValue', 55, function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.gadRead(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
+                nc._findDriver('gad', 'read')(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 55) {
@@ -537,11 +538,11 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('write() - device3', function (done) {
-        nc.gadWrite(permAddr3, 'generic/0', 'sensorValue', 88, function (err, result) {
+        nc._findDriver('gad', 'write')(permAddr3, 'generic/0', 'sensorValue', 88, function (err, result) {
             if (err) {
                 console.log(err);
             } else {
-                nc.gadRead(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
+                nc._findDriver('gad', 'read')(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
                     if (err) {
                         console.log(err);
                     } else if (result === 88) {
@@ -553,19 +554,19 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('write() - not found', function (done) {
-        nc.gadWrite(permAddr1, 'temperature/0', 'foo', 25, function (err, result) {
+        nc._findDriver('gad', 'write')(permAddr1, 'temperature/0', 'foo', 25, function (err, result) {
             if (err) done();
         });
     });
 
     it('write() - not allowed', function (done) {
-        nc.gadWrite(permAddr1, 'temperature/0', '5702', 25, function (err, result) {
+        nc._findDriver('gad', 'write')(permAddr1, 'temperature/0', '5702', 25, function (err, result) {
             if (err) done();
         });
     });
 
     it('exec()', function (done) {
-        nc.gadExec(permAddr1, 'temperature/0', '5704', ['Peter', 'KSHMR'], function (err, result) {
+        nc._findDriver('gad', 'exec')(permAddr1, 'temperature/0', '5704', ['Peter', 'KSHMR'], function (err, result) {
             if (err) {
                 console.log(err);
             } else {
@@ -575,32 +576,32 @@ describe('Gadget Drivers Check', function () {
     });
 
     it('exec() - not found', function (done) {
-        nc.gadExec(permAddr1, 'temperature/0', 'foo', [], function (err, result) {
+        nc._findDriver('gad', 'exec')(permAddr1, 'temperature/0', 'foo', [], function (err, result) {
             if (err) done();
         });
     });
 
     it('exec() - not allowed', function (done) {
-        nc.gadExec(permAddr1, 'temperature/0', 'sensorValue', [], function (err, result) {
+        nc._findDriver('gad', 'exec')(permAddr1, 'temperature/0', 'sensorValue', [], function (err, result) {
             if (err) done();
         });
     });
 
     var cfg;
 
-    it('setReportCfg()', function (done) {
+    it('writeReportCfg()', function (done) {
         cfg = {
             pmin: 0,
             pmax: 60,
             gt: 5
         };
 
-        nc.setReportCfg(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
             if (result === true) done();
         });
     });
 
-    it('setReportCfg() - enable true', function (done) {
+    it('writeReportCfg() - enable true', function (done) {
         var devReportHdlr = function (msg) {
                 switch(msg.type) {
                     case 'notify':
@@ -618,10 +619,10 @@ describe('Gadget Drivers Check', function () {
             enable: true
         };
 
-        nc.setReportCfg(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
             if (result === true) {
                 nc._controller.on('ind', devReportHdlr);
-                nc.gadWrite(permAddr1, 'temperature/0', 'sensorValue', 40, function (err, result) {
+                nc._findDriver('gad', 'write')(permAddr1, 'temperature/0', 'sensorValue', 40, function (err, result) {
                     if (err) console.log(err);
                 });
             }  
@@ -629,7 +630,7 @@ describe('Gadget Drivers Check', function () {
     });
 
 
-    it('setReportCfg() - enable true - device2', function (done) {
+    it('writeReportCfg() - enable true - device2', function (done) {
         var devReportHdlr = function (msg) {
                 switch(msg.type) {
                     case 'notify':
@@ -647,10 +648,10 @@ describe('Gadget Drivers Check', function () {
             enable: true
         };
 
-        nc.setReportCfg(permAddr2, 'humidity/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr2, 'humidity/0', 'sensorValue', cfg, function (err, result) {
             if (result === true) {
                 nc._controller.on('ind', devReportHdlr);
-                nc.gadWrite(permAddr2, 'humidity/0', 'sensorValue', 50, function (err, result) {
+                nc._findDriver('gad', 'write')(permAddr2, 'humidity/0', 'sensorValue', 50, function (err, result) {
                     if (err) console.log(err);
                 });
             }  
@@ -658,7 +659,7 @@ describe('Gadget Drivers Check', function () {
     });
 
 
-    it('setReportCfg() - enable true - device3', function (done) {
+    it('writeReportCfg() - enable true - device3', function (done) {
         var devReportHdlr = function (msg) {
                 switch(msg.type) {
                     case 'notify':
@@ -676,82 +677,82 @@ describe('Gadget Drivers Check', function () {
             enable: true
         };
 
-        nc.setReportCfg(permAddr3, 'generic/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr3, 'generic/0', 'sensorValue', cfg, function (err, result) {
             if (result === true) {
                 nc._controller.on('ind', devReportHdlr);
-                nc.gadWrite(permAddr3, 'generic/0', 'sensorValue', 80, function (err, result) {
+                nc._findDriver('gad', 'write')(permAddr3, 'generic/0', 'sensorValue', 80, function (err, result) {
                     if (err) console.log(err);
                 });
             }  
         });
     });
 
-    it('setReportCfg() - enable false', function (done) {
+    it('writeReportCfg() - enable false', function (done) {
         cfg = {
             enable: false
         };
 
-        nc.setReportCfg(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
             if (result === true) done();
         });
     });
 
-    it('setReportCfg() - attr not allowed', function (done) {
+    it('writeReportCfg() - attr not allowed', function (done) {
         cfg = {
             foo: false
         };
 
-        nc.setReportCfg(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr1, 'temperature/0', 'sensorValue', cfg, function (err, result) {
             if (err) done();
         });
     });
 
-    it('setReportCfg() - not found', function (done) {
+    it('writeReportCfg() - not found', function (done) {
         cfg = {
             pmin: 0,
             pmax: 60
         };
 
-        nc.setReportCfg(permAddr1, 'temperature/0', 'foo', cfg, function (err, result) {
+        nc._findDriver('gad', 'writeReportCfg')(permAddr1, 'temperature/0', 'foo', cfg, function (err, result) {
             if (err) done();
         });
     });
 
-    it('getReportCfg()', function (done) {
+    it('readReportCfg()', function (done) {
         cfg = {
             pmin: 0,
             pmax: 60,
         };
 
-        nc.getReportCfg(permAddr1, 'temperature/1', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'readReportCfg')(permAddr1, 'temperature/1', 'sensorValue', function (err, result) {
             if (_.isEqual(result, cfg)) done();
         });
     });
 
-    it('getReportCfg() - not found', function (done) {
-        nc.getReportCfg(permAddr1, 'temperature/1', 'foo', function (err, result) {
+    it('readReportCfg() - not found', function (done) {
+        nc._findDriver('gad', 'readReportCfg')(permAddr1, 'temperature/1', 'foo', function (err, result) {
             if (err) done();
         });
     });
 
-    it('getReportCfg() - device2', function (done) {
+    it('readReportCfg() - device2', function (done) {
         cfg = {
             pmin: 0,
             pmax: 60,
         };
 
-        nc.getReportCfg(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'readReportCfg')(permAddr2, 'humidity/0', 'sensorValue', function (err, result) {
             if (_.isEqual(result, cfg)) done();
         });
     });
 
-    it('getReportCfg() - device3', function (done) {
+    it('readReportCfg() - device3', function (done) {
         cfg = {
             pmin: 0,
             pmax: 60,
         };
 
-        nc.getReportCfg(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
+        nc._findDriver('gad', 'readReportCfg')(permAddr3, 'generic/0', 'sensorValue', function (err, result) {
             if (_.isEqual(result, cfg)) done();
         });
     });
